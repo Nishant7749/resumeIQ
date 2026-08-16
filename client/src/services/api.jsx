6b7881f911f8c788594b8f617bc8dcb897243582
Nishant Chaudhary <nish7749@gmail.com>
2026-08-16 22:31:41 +0530

    render configure

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://resumeiq-t2f1.onrender.com/'
 })
 
 export const postResume = async(file, role)=> {
